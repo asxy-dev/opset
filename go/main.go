d17fe4bae71c6bb8d4a78a9246d3636a678c0e5d
asxy-dev <lamsalamish1@gmail.com>
2025-11-13 00:43:54 +0800

    error debugged

--- go/main.go
+++ go/main.go
@@ -1,59 +1,65 @@
 package main
-import {
+
+import (
 	"fmt"
 	"encoding/json"
 	"os"
-}
+)
+
 type Server struct {
-	Name string `json:"name"`
-	Host string `json:"host"`
-	User string `json:"user"`
+	Name    string `json:"name"`
+	Host    string `json:"host"`
+	User    string `json:"user"`
 	KeyPath string `json:"key_path"`
 }
+
 type Task struct {
-	Command string `json:"command"`
+	Command  string `json:"command"`
 	Interval string `json:"interval"`
 }
-func main(){
+
+func main() {
 	if len(os.Args) < 2 { // to check user haave tyoer at least one argument
 		fmt.Println("Usage: opset <command> [options]") //show usage
 		return //exit
 	}
-	cmd := os.Args[1] //get the first argument 
+	cmd := os.Args[1] //get the first argument
 	switch cmd {
-		case "list-servers":
-			listServers() //call list server function
-		case "list-tasks":
-			listTasks() //call lsit task function
-		default:
-			fmt.Println("Unknown command:", cmd) //unknown command
-		}
+	case "list-servers":
+		listServers() //call list server function
+	case "list-tasks":
+		listTasks() //call lsit task function
+	default:
+		fmt.Println("Unknown command:", cmd) //unknown command
+	}
 }
+
 func listServers() {
 	data, err := os.ReadFile("data/servers.json")
-	if err != nil {	
+	if err != nil {
 		fmt.Println("Error reading servers file:", err)
-		return //end the program 
+		return //end the program
 	}
 
-var servers []Server //create slice to hold servers
+	var servers []Server //create slice to hold servers
 	json.Unmarshal(data, &servers)
-	fmt.Println("Servers:") 
+	fmt.Println("Servers:")
 	for _, server := range servers {
-	fmt.Println("- %s (%s)/n", server.Name, server.Host) //print server details
+		fmt.Printf("- %s (%s)\n", server.Name, server.Host) //print server details
 	}
 }
-func listTasks() {	
-	data , err := os.ReadFile("data/tasks.json")
+
+func listTasks() {
+	data, err := os.ReadFile("data/tasks.json")
 	if err != nil {
 		fmt.Println("Error reading tasks file:", err)
 		return //end the program
 	}
 
-var tasks []Task //create slice to hold tasks
+	var tasks []Task //create slice to hold tasks
 	json.Unmarshal(data, &tasks)
 	fmt.Println("Tasks:")
 	for _, task := range tasks {
-		fmt.Println("- %s (every %s)\n", task.Command, task.Interval) //print task details
+		fmt.Printf("- %s (every %s)\n", task.Command, task.Interval) //print task details
 	}
 }
