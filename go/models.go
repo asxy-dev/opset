785ed98244fcc356f95776f535d86b5a4455571f
asxy-dev <lamsalamish1@gmail.com>
2025-11-13 23:08:09 +0800

    wrote models.go (asxy)

--- go/models.go
+++ go/models.go
@@ -1,17 +1,23 @@
 package main
-
-// Server and Task model definitions
-
 type Server struct {
-	Name string `json:"name"`
-	Host string `json:"host"`
-	User string `json:"user"`
-	Port int    `json:"port"`
-}
+	Name string `json:"name"` //name of server
+	Host string `json:"host"` //ip or domain
+	User string `json:"user"` //ssh username (ubuntu etc)
+	KeyPath string `json:"key_path"` // path to ssh private key
 
-type Task struct {
-	Name    string   `json:"name"`
-	Command string   `json:"command"`
-	Targets []string `json:"targets"`
-	Cron    string   `json:"cron,omitempty"`
+	//server struct that hold information about remote machine
 }
+type Task struct {
+	Command string `json:"command"` // the shell command to execute
+	Interval string `json:"interval"` //time
+	
+	//task struct defines a schedule comand
+}
+type Result struct{
+	ServerName string //which server ran it
+	Command string //which command was executed
+	Output string //what came back (stdout)
+	Error string //any error message
+	TimeStamp string //when it ran
+}
+//complete
